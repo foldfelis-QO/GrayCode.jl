901203ea9a3eb8101921140910cc69834fb3bf4f
JingYu Ning <foldfelis@gmail.com>
2022-08-21 17:56:33 +0800

    Implement Gray code generator

--- src/GrayCode.jl
+++ src/GrayCode.jl
@@ -1,5 +1,21 @@
 module GrayCode
 
-# Write your package code here.
+export Gray, gen
+
+struct Gray{T<:Integer, N} end
+
+function gen_gray_codes(T, n)
+    (n == 1) && (return T[0, 1])
+
+    gray_codes = gen(Gray{T, n-1})
+    bitmask = one(T) << (n - 1)
+    for gray_code in reverse(gray_codes)
+        push!(gray_codes, bitmask | gray_code)
+    end
+
+    return gray_codes
+end
+
+gen(::Type{Gray{T, N}}) where {T, N} = gen_gray_codes(T, N)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,14 @@ using GrayCode
 using Test
 
 @testset "GrayCode.jl" begin
-    # Write your tests here.
+    @test bitstring.(gen(Gray{UInt8, 3})) == [
+        "00000000",
+        "00000001",
+        "00000011",
+        "00000010",
+        "00000110",
+        "00000111",
+        "00000101",
+        "00000100"
+    ]
 end
