45b9a3407bd57569d1d631101f16f841d0d1f6a6
JingYu Ning <foldfelis@gmail.com>
2022-08-21 23:38:30 +0800

    Implement primitive type

--- src/GrayCode.jl
+++ src/GrayCode.jl
@@ -1,21 +1,45 @@
 module GrayCode
 
-export Gray, gen
+export Gray, Gray8, Gray16, Gray32, Gray64, Gray128
 
-struct Gray{T<:Integer, N} end
+abstract type Gray <: Unsigned end
 
-function gen_gray_codes(T, n)
-    (n == 1) && (return T[0, 1])
+primitive type Gray8 <: Gray 8 end
+primitive type Gray16 <: Gray 16 end
+primitive type Gray32 <: Gray 32 end
+primitive type Gray64 <: Gray 64 end
+primitive type Gray128 <: Gray 128 end
 
-    gray_codes = gen(Gray{T, n-1})
-    bitmask = one(T) << (n - 1)
-    for gray_code in reverse(gray_codes)
-        push!(gray_codes, bitmask | gray_code)
+@inline _gray(x) = x ⊻ (x >> 1)
+@inline Gray8(x::UInt8) = reinterpret(Gray8, _gray(x))
+@inline Gray16(x::UInt16) = reinterpret(Gray16, _gray(x))
+@inline Gray32(x::UInt32) = reinterpret(Gray32, _gray(x))
+@inline Gray64(x::UInt64) = reinterpret(Gray64, _gray(x))
+@inline Gray128(x::UInt128) = reinterpret(Gray128, _gray(x))
+
+@inline reinterpret2Int(g::Gray8) = reinterpret(UInt8, g)
+@inline reinterpret2Int(g::Gray16) = reinterpret(UInt16, g)
+@inline reinterpret2Int(g::Gray32) = reinterpret(UInt32, g)
+@inline reinterpret2Int(g::Gray64) = reinterpret(UInt64, g)
+@inline reinterpret2Int(g::Gray128) = reinterpret(UInt128, g)
+@inline function _int(g::T) where {T<:Gray}
+    x = reinterpret2Int(g)
+    mask = x >> 1
+    while mask > 0
+        x ⊻= mask
+        mask >>= 1
     end
 
-    return gray_codes
+    return x
 end
 
-gen(::Type{Gray{T, N}}) where {T, N} = gen_gray_codes(T, N)
+Base.show(io::IO, g::Gray) = print(io, "$(typeof(g))($(_int(g)))")
+
+@inline Gray(x::UInt8) = Gray8(x)
+@inline Gray(x::UInt16) = Gray16(x)
+@inline Gray(x::UInt32) = Gray32(x)
+@inline Gray(x::UInt64) = Gray64(x)
+@inline Gray(x::UInt128) = Gray128(x)
+@inline Gray(x::Integer) = Gray(unsigned(x))
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,16 @@
 using GrayCode
+using InteractiveUtils
 using Test
 
+const Us = [UInt128, UInt16, UInt32, UInt64, UInt8]
+const Is = [Int128, Int16, Int32, Int64, Int8]
+const Gs = [Gray128, Gray16, Gray32, Gray64, Gray8]
+
 @testset "GrayCode.jl" begin
-    @test bitstring.(gen(Gray{UInt8, 3})) == [
-        "00000000",
-        "00000001",
-        "00000011",
-        "00000010",
-        "00000110",
-        "00000111",
-        "00000101",
-        "00000100"
-    ]
+    for (U, I, G) in zip(Us, Is, Gs)
+        for i in typemin(UInt8):typemax(UInt8)
+            @test GrayCode._int(Gray(U(i))) == U(i)
+            (reinterpret(Int8, i) < 0) || @test GrayCode._int(Gray(I(i))) == U(i)
+        end
+    end
 end
