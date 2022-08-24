a336b71a9ee27f9d14909b5b4c96a3791d4bd935
Documenter.jl <documenter@juliadocs.github.io>
2022-08-24 15:56:12 +0000

    build based on 7bc45b0

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GrayCode","category":"page"},{"location":"#GrayCode","page":"Home","title":"GrayCode","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GrayCode.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GrayCode]","category":"page"}]
+}
